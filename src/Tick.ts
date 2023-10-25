71c0728a3ce6a84f55a980cfb6717780158fe1c6
Mizab <mizabmansari@gmail.com>
2023-10-25 21:39:49 +0530

    Added Hurt Logic

--- src/Tick.ts
+++ src/Tick.ts
@@ -1,6 +1,14 @@
-import { MCFunction, MCFunctionInstance, Selector } from "sandstone";
+import {
+  MCFunction,
+  MCFunctionInstance,
+  NBT,
+  Selector,
+  execute,
+} from "sandstone";
 import { HorseSword } from "./Swords/HorseSword/HorseSword";
 import { BlazeSword } from "./Swords/BlazeSword/BlazeSword";
+import { SquidSword } from "./Swords/SquidSword/SquidSword";
+import { SquidHurtEffect } from "./Swords/SquidSword/HurtEffect";
 
 // Self Context
 export const self = Selector("@s");
@@ -8,8 +16,18 @@ export const self = Selector("@s");
 const Tick: MCFunctionInstance = MCFunction(
   "tick",
   () => {
+    // Main Logic
     HorseSword();
     BlazeSword();
+    SquidSword();
+
+    // Execute when a player hurts another player
+    execute
+      .as(Selector("@a", { nbt: { HurtTime: NBT.short(10) } }))
+      .at(self)
+      .run(() => {
+        SquidHurtEffect();
+      });
   },
   {
     runEachTick: true,
