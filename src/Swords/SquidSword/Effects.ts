7055a380da4509cd6b6e54370eaad96db87246eb
Mizab <mizabmansari@gmail.com>
2023-10-25 21:56:20 +0530

    Fixed typo

--- src/Swords/SquidSword/Effects.ts
+++ src/Swords/SquidSword/Effects.ts
@@ -2,7 +2,7 @@ import { MCFunction, MCFunctionInstance, effect } from "sandstone";
 import { self } from "../../Tick";
 
 export const applyAquaticPowers: MCFunctionInstance = MCFunction(
-  "swords/aquatic_sword/apply_aquatic_powers",
+  "swords/squid_sword/apply_aquatic_powers",
   () => {
     effect.give(self, "minecraft:water_breathing", 3, 4, true);
     effect.give(self, "minecraft:dolphins_grace", 2, 4, true);
