30d553ce1cd0726c5d196dd33e410db009947236
Mizab <mizabmansari@gmail.com>
2023-10-26 17:19:18 +0530

    Rename result file

--- src/Swords/CreeperSword/Effects.ts
+++ src/Swords/CreeperSword/Effects.ts
@@ -2,7 +2,7 @@ import { MCFunction, MCFunctionInstance, effect } from "sandstone";
 import { self } from "../../Tick";
 
 export const applyCreeperPowers: MCFunctionInstance = MCFunction(
-  "swords/creeper_sword/apply_aquatic_powers",
+  "swords/creeper_sword/apply_creeper_powers",
   () => {
     effect.give(self, "minecraft:resistance", 3, 1, true);
   }
